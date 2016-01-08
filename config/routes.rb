25057f699d0f8d38f72b8e57f98e547774d5c69f
James Keane <jpkeane@me.com>
2016-01-08 15:52:27 +0000

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  jsonapi_resources :contacts
+  jsonapi_resources :phone_numbers
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
